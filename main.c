71b8b60fc63aecb38c6fe4b9f5a246de5dc216e8
Yann Sionneau <yann@minet.net>
2012-12-20 09:06:23 +0100

    Fix memory and process leak
    A wait() was missing in the function
    that forks() to execute a shell command in
    a subprocess.
    
    If we don't wait() for dying children,
    they will stay in Zombie state forever
    sucking up memory and PID of the system.

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #ifdef DEBUG_ENABLED
 #define debug(fmt, ...) printf(fmt, ##__VA_ARGS__)
@@ -35,6 +37,7 @@ char *exec_command(char *cmd)
 	ssize_t size;
 	ssize_t total_read;
 	ssize_t output_size;
+	int status;
 
 	debug("Executing: %s\n", cmd);
 
@@ -94,6 +97,10 @@ char *exec_command(char *cmd)
 
 	command_output[total_read] = '\0';
 	close(pipefd[READ_PIPE]);
+	if ( wait(&status) != pid ) // Wait for the child to die
+	{
+		perror("child process error:");
+	}
 
 	debug("[%p] => %s\n", command_output, command_output);
 
